45378b26ac8926f2739c87b0a4e54701147b7af7
fluo4 <fluo4real@gmail.com>
2025-08-11 14:45:16 -0400

    Update Resilience4j

--- employee-service/src/main/java/flxjrllc/javaspringboot/employee_service/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/flxjrllc/javaspringboot/employee_service/service/impl/EmployeeServiceImpl.java
@@ -7,7 +7,11 @@ import flxjrllc.javaspringboot.employee_service.entity.Employee;
 import flxjrllc.javaspringboot.employee_service.repository.EmployeeRepository;
 import flxjrllc.javaspringboot.employee_service.service.APIClient;
 import flxjrllc.javaspringboot.employee_service.service.EmployeeService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -17,10 +21,12 @@ import org.springframework.web.reactive.function.client.WebClient;
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeServiceImpl.class);
+
     private EmployeeRepository employeeRepository;
 
 //    private RestTemplate restTemplate;
-//    private WebClient webClient;
+    private WebClient webClient;
     private APIClient apiClient;
 
     @Override
@@ -48,20 +54,24 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     }
 
+    @Retry(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
+    @CircuitBreaker(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
     @Override
     public APIResponseDto getEmployeeById(Long employeeId) {
+        LOGGER.info("Inside getEmployeeById() method");
+
         Employee employee = employeeRepository.findById(employeeId).get();
 
 //        ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/departments/" + employee.getDepartmentCode(), DepartmentDto.class);
 //        DepartmentDto departmentDto = responseEntity.getBody();
 
-//        DepartmentDto departmentDto = webClient.get()
-//        .uri("http://localhost:8080/api/departments/" + employee.getDepartmentCode())
-//        .retrieve()
-//        .bodyToMono(DepartmentDto.class)
-//        .block();
+        DepartmentDto departmentDto = webClient.get()
+        .uri("http://localhost:8080/api/departments/" + employee.getDepartmentCode())
+        .retrieve()
+        .bodyToMono(DepartmentDto.class)
+        .block();
 
-        DepartmentDto departmentDto = apiClient.getDepartment(employee.getDepartmentCode());
+//        DepartmentDto departmentDto = apiClient.getDepartment(employee.getDepartmentCode());
 
         EmployeeDto employeeDto = new EmployeeDto(
                 employee.getId(),
@@ -78,5 +88,29 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         return apiResponseDto;
 
+    }
+
+    public APIResponseDto getDefaultDepartment(Long employeeId, Exception exception) {
+        LOGGER.info("fall back method");
+        Employee employee = employeeRepository.findById(employeeId).get();
+
+        DepartmentDto departmentDto = new DepartmentDto();
+        departmentDto.setDepartmentName("Warriors");
+        departmentDto.setDepartmentCode("GSW");
+        departmentDto.setDepartmentDescription("Golden State Warriors");
+
+        EmployeeDto employeeDto = new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail(),
+                employee.getDepartmentCode()
+        );
+
+        APIResponseDto apiResponseDto = new APIResponseDto();
+        apiResponseDto.setEmployee(employeeDto);
+        apiResponseDto.setDepartment(departmentDto);
+
+        return apiResponseDto;
     }
 }
